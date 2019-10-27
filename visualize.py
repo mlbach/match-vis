f6b75e046efcd3fcae962b1320304365a7b5faf4
mlbach <38857773+mlbach@users.noreply.github.com>
2019-10-27 15:27:24 +0100

    Added option to sequentialyl process files with same name prefix

--- visualize.py
+++ visualize.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 from random import randint
 import argparse
+import pathlib
 
 
 def split_convert_line(line_str):
@@ -31,16 +32,19 @@ def draw_clock(img, elapsed_time, position=(50, 50)):
                 (tlx + 5, tly + 33), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
                 
 
-def visualize(blank_image, ts, save_video, vid_fps):
+def visualize(blank_image, ts, filename, save_video, vid_fps):
 
 
     if save_video:
         vid_path, vid_writer = None, None
         
-        if fn.find("/") > -1:
-            filename = fn.split("/")[-1] #without folders
+        
+        if filename.find("/") > -1:
+            filename = filename.split("/")[-1] #without folders
             
+        print(filename)
         vid_path = 'videos/'+filename[:-3]+'avi' #avi
+        print(vid_path)
         '''if isinstance(vid_writer, cv2.VideoWriter):
             vid_writer.release()  # release previous video writer
         '''
@@ -80,6 +84,7 @@ def visualize(blank_image, ts, save_video, vid_fps):
             vid_writer.write(base_img)
         else:
             cv2.imshow('image', base_img)
+            cv2.imshow('focus', base_img)
             if x == len(ts)-1:
                 cv2.waitKey(0)
             else:
@@ -96,8 +101,26 @@ def get_elapsed_time(starttime, now):
     return "%02d:%02d" % (minutes[0], seconds[0])
  
 
+def get_filenames(example_filename):
 
+    if example_filename.find("/") > -1:
+        path = example_filename.split("/") #seperate path, filename 
+        dir = "/".join(path[:-1])
+        file = path[-1]
+    else:
+        dir = "."
+        file = example_filename
 
+    file = file.split("_")
+    file = "_".join(file[:2])
+    file += "_*.log" # filename_*suffix*.log
+
+    files = pathlib.Path(dir).glob(file) #find all files that match name
+
+    l = []
+    for f in files:
+        l.append(f.as_posix().__str__())
+    return l
 
 
 
@@ -106,6 +129,7 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     parser.add_argument('--match-logs', type=str, default=None, help='match log file to visualize')
+    parser.add_argument('--file-sequence', type=bool, default=False, help='true if you want to sequentially load multiple files of the same base name')
     parser.add_argument('--save-video', type=bool, default=False, help='false to display animation, true to save to video')
     parser.add_argument('--player-focus', type=str, default=None, help='expects None or valid externalID of object. if object ID is given, display second screen and follow movement')
     opt = parser.parse_args()
@@ -114,60 +138,65 @@ if __name__ == '__main__':
     if opt.match_logs == None:
         raise ValueError('No filename of match logs was given!')
     fn = opt.match_logs
-    print(fn)
-    print(fn.split("/"))
-
-    ts = []
-    # read data from file
-    with open(fn) as f:
-        #ts = [split_convert_line(next(f).strip()) for x in range(100)] #read only x lines
-        for line in f:
-            ts.append(split_convert_line(line))
-
-    #print(len(ts))
-    print(ts[:5])
+    fn = fn.replace("\\", "/") #unify directory seperator
     
-    realtime_start = datetime.strptime(ts[0][1], '%d %B %Y %H:%M:%S.%f')        # Random date in the past
-    realtime_stop  = datetime.strptime(ts[-1][1], '%d %B %Y %H:%M:%S.%f')                         # Now
-    realtime_duration = realtime_stop - realtime_start                        # For build-in functions
-    rt_duration_in_s = realtime_duration.total_seconds()
-    #print(rt_duration_in_s)
-    minutes = divmod(rt_duration_in_s, 60)           # Use remainder of hours to calc minutes
-    seconds = divmod(minutes[1], 1)               # Use remainder of minutes to calc seconds
-    print("Real match duration= %d:%d" % (minutes[0], seconds[0]))
-    
-    if opt.save_video:
-        vid_fps = int(round(len(ts) / rt_duration_in_s, 0)) 
-        print("Frame rate", vid_fps)
+    if opt.file_sequence:
+        files = get_filenames(fn)
     else:
-        vid_fps = None
-    
-    
+        files = [fn]
 
-    scf = 10 * 2
-    offset_y = 200
-    offset_x = 50
-    border_top = int(round(offset_y/2, 0))
-    border_left = int(round(offset_x/2, 0))
+    for file in files:
+        ts = []
+        # read data from file
+        with open(file) as f:
+            #ts = [split_convert_line(next(f).strip()) for x in range(100)] #read only x lines
+            for line in f:
+                ts.append(split_convert_line(line))
 
-    # bild dimensionen
-    h = 20 * scf + offset_y #height
-    w = 40 * scf + offset_x #width 40 * 10 * 2
+        #print(len(ts))
+        print(ts[:5])
+        
+        realtime_start = datetime.strptime(ts[0][1], '%d %B %Y %H:%M:%S.%f')        # Random date in the past
+        realtime_stop  = datetime.strptime(ts[-1][1], '%d %B %Y %H:%M:%S.%f')                         # Now
+        realtime_duration = realtime_stop - realtime_start                        # For build-in functions
+        rt_duration_in_s = realtime_duration.total_seconds()
+        #print(rt_duration_in_s)
+        minutes = divmod(rt_duration_in_s, 60)           # Use remainder of hours to calc minutes
+        seconds = divmod(minutes[1], 1)               # Use remainder of minutes to calc seconds
+        print("Real match duration= %d:%d" % (minutes[0], seconds[0]))
+        
+        if opt.save_video:
+            vid_fps = int(round(len(ts) / rt_duration_in_s, 0)) 
+            print("Frame rate", vid_fps)
+        else:
+            vid_fps = None
+        
+        
 
-    blank_image = np.zeros((h, w, 3), np.uint8)
-    blank_image[:, :, 1 ] = 204 #0, 204, 0 #rgb
+        scf = 10 * 2
+        offset_y = 200
+        offset_x = 50
+        border_top = int(round(offset_y/2, 0))
+        border_left = int(round(offset_x/2, 0))
 
-    #spielfelddimensionen
-    cv2.rectangle(blank_image, (border_left, border_top), 
-    (w - border_left, h - border_top), (250,250,250))
-    
-    displaytime_start = datetime.now()
-    
-    visualize(blank_image, ts, opt.save_video, vid_fps) #display animation
-    
-    displaytime_stop = datetime.now()
-    displaytime_duration = displaytime_stop - displaytime_start
-    dt_duration_in_s = displaytime_duration.total_seconds()
-    minutes = divmod(dt_duration_in_s, 60)
-    seconds = divmod(minutes[1], 1)
-    print("Display match duration= %d:%d" % (minutes[0], seconds[0]))
+        # bild dimensionen
+        h = 20 * scf + offset_y #height
+        w = 40 * scf + offset_x #width 40 * 10 * 2
+
+        blank_image = np.zeros((h, w, 3), np.uint8)
+        blank_image[:, :, 1 ] = 204 #0, 204, 0 #rgb
+
+        #spielfelddimensionen
+        cv2.rectangle(blank_image, (border_left, border_top), 
+        (w - border_left, h - border_top), (250,250,250))
+        
+        displaytime_start = datetime.now()
+        
+        visualize(blank_image, ts, file, opt.save_video, vid_fps) #display animation
+        
+        displaytime_stop = datetime.now()
+        displaytime_duration = displaytime_stop - displaytime_start
+        dt_duration_in_s = displaytime_duration.total_seconds()
+        minutes = divmod(dt_duration_in_s, 60)
+        seconds = divmod(minutes[1], 1)
+        print("Display match duration= %d:%d" % (minutes[0], seconds[0]))
