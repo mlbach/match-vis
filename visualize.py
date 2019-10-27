ded93f84bb41f6d877343efb91230e5709a3b550
mlbach <38857773+mlbach@users.noreply.github.com>
2019-10-27 17:06:11 +0100

    Cleaned up code

--- visualize.py
+++ visualize.py
@@ -19,7 +19,7 @@ def split_convert_line(line_str):
     return line + lst
 
 
-def draw_clock(img, elapsed_time, position=(50, 50)):
+def draw_clock(img, visual_params, elapsed_time, position=(50, 50)):
 
     tlx = position[0] # top left position, x-axis
     tly = position[1] # top left position y-axis
@@ -32,18 +32,20 @@ def draw_clock(img, elapsed_time, position=(50, 50)):
                 (tlx + 5, tly + 33), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
                 
 
-def visualize(blank_image, ts, filename, save_video, vid_fps, player_focus):
+def visualize(blank_image, ts, filename, visual_params, player_focus):
+    padding_x = visual_params["offset_x"] * visual_params["magnitude"]
+    padding_y = visual_params["offset_y"] * visual_params["magnitude"]
+    
+    border_top = int(round(padding_y/2, 0))
+    border_left = int(round(padding_x/2, 0))
+    
+    h = 20 * visual_params["magnitude"] * visual_params["scf"] + padding_y #height
+    w = 40 * visual_params["magnitude"] * visual_params["scf"] + padding_x #width 40 * 10 * 2
     
     if opt.player_focus != None:
-        focus_image = np.zeros((40 * 10 + 25, 20 * 10 + 25, 3), np.uint8) #construct second image for player focus, dimensions rotated
-        focus_image[:, :, 1] = 204
+        focus_pitch = construct_focus_pitch(player_focus, visual_params)
         
-        cv2.putText(focus_image, "Focus on player "+player_focus, (10, 10), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 1)
-        
-        cv2.rectangle(focus_image, (12, 12), 
-        (focus_image.shape[1] - 13, focus_image.shape[0] - 13), (250,250,250))
-        
-    if save_video:
+    if visual_params["save_video"]:
         vid_path, vid_writer = None, None
         
         
@@ -59,7 +61,7 @@ def visualize(blank_image, ts, filename, save_video, vid_fps, player_focus):
         width = blank_image.shape[1]
         height = blank_image.shape[0]
         
-        vid_writer = cv2.VideoWriter(vid_path, cv2.VideoWriter_fourcc(*'XVID'), vid_fps, (width, height)) #avc1
+        vid_writer = cv2.VideoWriter(vid_path, cv2.VideoWriter_fourcc(*'XVID'), visual_params["vid_fps"], (width, height)) #avc1
         
         print("Saving the match visualization to video.")
     
@@ -69,12 +71,12 @@ def visualize(blank_image, ts, filename, save_video, vid_fps, player_focus):
         base_img = blank_image.copy()
         now = datetime.strptime(ts[x][1], '%d %B %Y %H:%M:%S.%f')
     
-        draw_clock(base_img, get_elapsed_time(starttime, now), (int(round(w/2, 0)) - 50,25))
+        draw_clock(base_img, visual_params, get_elapsed_time(starttime, now), (int(round(w/2, 0)) - 50,25))
         
         for obj in range(2, len(ts[x])):
             
-            x_t = int(round(ts[x][obj][2] * scf)) + border_top
-            y_t = int(round(ts[x][obj][3] * scf)) + border_left
+            x_t = int(round(ts[x][obj][2] * visual_params["scf"] * visual_params["magnitude"])) + border_top
+            y_t = int(round(ts[x][obj][3] * visual_params["scf"] * visual_params["magnitude"])) + border_left
             
             if ts[x][obj][1][0] == "A":
                 type = "player"
@@ -87,23 +89,28 @@ def visualize(blank_image, ts, filename, save_video, vid_fps, player_focus):
                 cv2.putText(base_img, number, (y_t - 10, x_t + 5), cv2.FONT_HERSHEY_PLAIN, 1, (250,250,250), 2)
                 
                 if ts[x][obj][1] == player_focus:
-                    cv2.circle(focus_image, (int(x_t/2), int(y_t/2)), 1, (250, 0, 0), 1)
+                    cv2.circle(focus_pitch, 
+                            (int(round((ts[x][obj][2] * visual_params["scf"] * visual_params["magnitude"]  + border_left)/2)), #focus pitch uses equal padding
+                            int(round((ts[x][obj][3] * visual_params["scf"] * visual_params["magnitude"]  + border_left)/2))),
+                            1, (250, 0, 0), 1)
                 
             else:
                 cv2.circle(base_img, (y_t, x_t),8, (250, 250, 250), -1) #ball
                 
-        if save_video:
+        if visual_params["save_video"]:
             vid_writer.write(base_img)
         else:
             cv2.imshow('image', base_img)
             if player_focus != None:
-                cv2.imshow('focus', focus_image)
+                flipped_focus = cv2.flip(focus_pitch.copy(), 0)
+                cv2.putText(flipped_focus, "Focus on player "+player_focus, (10, 10), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 1)
+                cv2.imshow('focus', flipped_focus)
             if x == len(ts)-1:
                 cv2.waitKey(0)
             else:
                 cv2.waitKey(1)
                 
-    if save_video:
+    if visual_params["save_video"]:
         vid_writer.release()
 
 def get_elapsed_time(starttime, now):
@@ -135,8 +142,42 @@ def get_filenames(example_filename):
         l.append(f.as_posix().__str__())
     return l
 
+def construct_pitch(params):
+    padding_x = params["offset_x"] * params["magnitude"]
+    padding_y = params["offset_y"] * params["magnitude"]
+    
+    border_top = int(round(padding_y/2, 0))
+    border_left = int(round(padding_x/2, 0))
 
+    # bild dimensionen
+    h = 20 * params["magnitude"] * params["scf"] + padding_y #height
+    w = 40 * params["magnitude"] * params["scf"] + padding_x #width 40 * 10 * 2
 
+    blank_image = np.zeros((h, w, 3), np.uint8)
+    blank_image[:, :, 1 ] = 204 #0, 204, 0 #rgb
+
+    #spielfelddimensionen
+    cv2.rectangle(blank_image, (border_left, border_top), 
+    (w - border_left, h - border_top), (250,250,250))
+    
+    return blank_image
+
+def construct_focus_pitch(focus, params):
+        focus_image = np.zeros((40 * 10 + 25, 20 * 10 + 25, 3), np.uint8) #construct second image for player focus, dimensions rotated
+        focus_image[:, :, 1] = 204
+        
+        cv2.rectangle(focus_image, (12, 12), 
+        (focus_image.shape[1] - 13, focus_image.shape[0] - 13), (250,250,250))
+        
+        return focus_image
+
+def calc_duration(datetime_start, datetime_stop):
+    duration = datetime_start - datetime_stop                        # For build-in functions
+    duration_in_s = duration.total_seconds()
+    minutes = divmod(duration_in_s, 60)           # Use remainder of hours to calc minutes
+    seconds = divmod(minutes[1], 1)               # Use remainder of minutes to calc seconds 
+    
+    return minutes[0], seconds[0]
 
 if __name__ == '__main__':
     
@@ -162,54 +203,35 @@ if __name__ == '__main__':
         ts = []
         # read data from file
         with open(file) as f:
-            #ts = [split_convert_line(next(f).strip()) for x in range(100)] #read only x lines
             for line in f:
                 ts.append(split_convert_line(line))
 
         #print(len(ts))
         print(ts[:5])
         
-        realtime_start = datetime.strptime(ts[0][1], '%d %B %Y %H:%M:%S.%f')        # Random date in the past
-        realtime_stop  = datetime.strptime(ts[-1][1], '%d %B %Y %H:%M:%S.%f')                         # Now
-        realtime_duration = realtime_stop - realtime_start                        # For build-in functions
-        rt_duration_in_s = realtime_duration.total_seconds()
-        #print(rt_duration_in_s)
-        minutes = divmod(rt_duration_in_s, 60)           # Use remainder of hours to calc minutes
-        seconds = divmod(minutes[1], 1)               # Use remainder of minutes to calc seconds
-        print("Real match duration= %d:%d" % (minutes[0], seconds[0]))
+        #calculate actual match duration
+        min, sec = calc_duration(datetime.strptime(ts[-1][1], '%d %B %Y %H:%M:%S.%f'), datetime.strptime(ts[0][1], '%d %B %Y %H:%M:%S.%f')) #datetime from first, last timesteps
+        print("Real match duration= %d:%d" % (min, sec))
         
+        visual_parameters = {
+            "pitch_height" : 20,
+            "pitch_width" : 40,
+            "magnitude" : 10,
+            "scf" : 2, #scaling factor
+            "offset_y" : 20,
+            "offset_x" : 5,
+            "save_video" : opt.save_video,
+            "vid_fps" : None
+        }
         if opt.save_video:
-            vid_fps = int(round(len(ts) / rt_duration_in_s, 0)) 
-            print("Frame rate", vid_fps)
-        else:
-            vid_fps = None
-        
-        
-        scf = 10 * 2
-        offset_y = 200
-        offset_x = 50
-        border_top = int(round(offset_y/2, 0))
-        border_left = int(round(offset_x/2, 0))
+            visual_parameters["vid_fps"] = int(round(len(ts) / rt_duration_in_s, 0)) 
 
-        # bild dimensionen
-        h = 20 * scf + offset_y #height
-        w = 40 * scf + offset_x #width 40 * 10 * 2
-
-        blank_image = np.zeros((h, w, 3), np.uint8)
-        blank_image[:, :, 1 ] = 204 #0, 204, 0 #rgb
-
-        #spielfelddimensionen
-        cv2.rectangle(blank_image, (border_left, border_top), 
-        (w - border_left, h - border_top), (250,250,250))
-        
+        pitch = construct_pitch(visual_parameters)
         
         displaytime_start = datetime.now()
         
-        visualize(blank_image, ts, file, opt.save_video, vid_fps, opt.player_focus) #display animation
+        visualize(pitch, ts, file, visual_parameters, opt.player_focus) #display animation
         
-        displaytime_stop = datetime.now()
-        displaytime_duration = displaytime_stop - displaytime_start
-        dt_duration_in_s = displaytime_duration.total_seconds()
-        minutes = divmod(dt_duration_in_s, 60)
-        seconds = divmod(minutes[1], 1)
-        print("Display match duration= %d:%d" % (minutes[0], seconds[0]))
+        #calculate time it took to display visualization
+        min, sec = calc_duration(datetime.now(), displaytime_start)
+        print("Display match duration= %d:%d" % (min, sec))
